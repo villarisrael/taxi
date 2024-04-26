9de048eedf4f935b3cc819eb4ed86ff347741f5d
Uziel Marquez <uzielmarquezgarcia@gmail.com>
2024-04-26 17:41:48 -0600

    Agregue PDF con formato predefinido para el documento, borrador 1

--- Certificado2/Controllers/MonedasController.cs
+++ Certificado2/Controllers/MonedasController.cs
@@ -8,16 +8,19 @@ using iText.Layout.Element;
 using iText.Layout.Properties;
 using iText.Layout;
 using Microsoft.AspNetCore.Mvc;
+using Certificado2.Modelos;
 
 namespace Certificado2.Controllers
 {
     public class MonedasController : Controller
     {
         private readonly IRepositorioMonedas _repositorioMonedas;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public MonedasController(IRepositorioMonedas repositorioMonedas)
+        public MonedasController(IRepositorioMonedas repositorioMonedas, IWebHostEnvironment webHostEnvironment)
         {
             _repositorioMonedas = repositorioMonedas;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult BuscarCertificado()
@@ -31,129 +34,55 @@ namespace Certificado2.Controllers
             return View();
         }
 
-        public async Task<IActionResult> GenerarPdf()
+        public async Task<IActionResult> GenerarPdf(string Serie, string Folio)
         {
             //Variable string igualado con DateTime para obtener la fecha
             string date1 = DateTime.UtcNow.ToString("dd-MM-yyyy");
 
-            //// Se obtiene la lista completa de rutas
-            //var listadoRutas = await _repositorioMonedas.ObtenerListado();
 
-            //// Obtener los bytes del logo
+            string imageName = "certificado2.png";
 
-            // Obtener el logotipo de la empresa
-            //byte[] logotipoBytes = await _repositorioMonedas.ObtenerLogotipoEmpresaAsync();
-
-
-
-            // Se crea un MemoryStream para almacenar el PDF generado
+            // Ruta relativa de la imagen dentro de wwwroot
+            string imagePath = System.IO.Path.Combine(_webHostEnvironment.WebRootPath, "imagenes", imageName);
+            
             MemoryStream ms = new MemoryStream();
 
 
 
-            // Se crea un escritor PDF y un documento PDF utilizando iTextSharp
+            
 
             PdfWriter pw = new PdfWriter(ms);
             PdfDocument pdfDocument = new PdfDocument(pw);
             Document doc = new Document(pdfDocument, PageSize.LETTER);
+
+            
+            Image formatImage = new Image(ImageDataFactory.Create(imagePath));
+
+            
+            formatImage.SetAutoScale(true);
+
+            
+            doc.Add(formatImage);
+
+
             doc.SetMargins(75, 35, 70, 35);
 
+            Paragraph paragraph = new Paragraph($"CERTIFICADO_DE_AUTENTICIDAD.COM")
+            .SetFixedPosition(130, 700, 400) // Establece la posición del texto sobre la imagen
+            .SetTextAlignment(TextAlignment.LEFT) // Alinea el texto
+            .SetFontSize(12) // Establece el tamaño de fuente
+            .SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK); // Establece el color de fuente
 
-            doc.Add(new Paragraph("COMISION DE AGUA Y ALCANTARILLADO DEL MUNICIPIO DE ACTOPAN, HIDALGO").SetTextAlignment(TextAlignment.CENTER).SetFontSize(11));
-            doc.Add(new Paragraph(date1).SetTextAlignment(TextAlignment.RIGHT).SetFontSize(9));
-
-            //// Obtener el nombre de la empresa desde la base de datos
-            //string nombreEmpresa = await _repositorioMonedas.ObtenerNombreEmpresaAsync();
-
-
-
-            // Agregar el nombre de la empresa al documento PDF
-            doc.Add(new Paragraph("EMPRESA").SetTextAlignment(TextAlignment.CENTER).SetFontSize(10).SetBold());
+            doc.Add(paragraph);
 
 
-            //if (logotipoBytes == null || logotipoBytes.Length == 0)
-            //{
-            //    // Manejar el caso en que el logotipo no se cargue correctamente
-            //    return BadRequest("No se pudo cargar el logotipo de la empresa.");
-            //}
+            Paragraph paragraphSerie = new Paragraph($"{Serie} {Folio}")
+            .SetFixedPosition(460, 700, 400) // Establece la posición del texto sobre la imagen
+            .SetTextAlignment(TextAlignment.LEFT) // Alinea el texto
+            .SetFontSize(12) // Establece el tamaño de fuente
+            .SetFontColor(iText.Kernel.Colors.ColorConstants.BLACK); // Establece el color de fuente
 
-            //// Convertir el logotipo a ImageData para usarlo en el PDF
-            //ImageData logotipoData = ImageDataFactory.Create(logotipoBytes);
-
-            // Crear el documento PDF y agregar el logotipo
-            //Image logo = new Image(logotipoData).ScaleToFit(50, 50); // Ajusta el tamaño según sea necesario
-            //doc.Add(logo);
-
-
-            doc.Add(new Paragraph("\n"));
-            Table table1 = new Table(1).UseAllAvailableWidth();
-            Cell cell1 = new Cell().Add(new Paragraph("Reporte de Rutas").SetFontSize(9).SetTextAlignment(TextAlignment.CENTER).SetBorder(Border.NO_BORDER));
-            table1.AddCell(cell1);
-            doc.Add(table1);
-
-            Style styleCell = new Style().SetBackgroundColor(ColorConstants.LIGHT_GRAY).SetFontSize(9);
-            //aqui se genera la tabla 
-            Table _table = new Table(10).UseAllAvailableWidth();
-            Cell _cell = new Cell().Add(new Paragraph("#"));
-            // Se añaden los encabezados de la tabla
-
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("IDRuta"));
-
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("Ruta"));
-
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("IDRegion"));
-
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("TomaLec"));
-
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("Capturas"));
-
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("Calculo"));
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("Aplicacion"));
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("Cierre"));
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-            _cell = new Cell().Add(new Paragraph("Num Usuarios"));
-            _table.AddHeaderCell(_cell.AddStyle(styleCell));
-
-            //List<Rutas> model = listadoRutas.Where(ruta => !string.IsNullOrEmpty(ruta.Id)).ToList();
-
-
-            //int x = 0;
-            //foreach (var item in model)
-            //{
-            //    x++;
-            //    _cell = new Cell().Add(new Paragraph(x.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.Id.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.Nombre.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.IdRegion.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.TomaLec.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.Captura.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.Calculo.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.Aplicacion.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.Cierre.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-            //    _cell = new Cell().Add(new Paragraph(item.NUsuarios.ToString()).SetFontSize(8));
-            //    _table.AddCell(_cell);
-
-
-            //}
-
-            doc.Add(_table);
+            doc.Add(paragraphSerie);
 
             doc.Close();
 
