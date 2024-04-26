83e5eb83176a394e07972226b971159ff60a8a5d
ISRAEL VILLAR <villar_israel@yahoo.com>
2024-04-26 10:21:36 -0600

    modelo Monedas

---
+++ Certificado2/Modelos/Moenda.cs
@@ -0,0 +1,20 @@
+﻿namespace Certificado2.Modelos
+{
+    using System;
+
+    public class Moneda
+    {
+        public int IdCertificado { get; set; }
+        public string Serie { get; set; }
+        public int Folio { get; set; }
+        public int? IdCertificador { get; set; }
+        public int? IdUsuario { get; set; }
+        public string Nombre { get; set; }
+        public int? Ano { get; set; }
+        public string Ceca { get; set; }
+        public string Material { get; set; }
+        public string Estado { get; set; }
+        public byte[] Foto { get; set; }
+    }
+
+}
